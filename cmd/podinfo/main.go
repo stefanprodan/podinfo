09a743f5c2cf09dd664261b06f7e3485099c022d
Stefan Prodan <stefan.prodan@gmail.com>
2018-08-10 11:48:12 +0300

    Add CPU and Memory stress test flags

--- cmd/podinfo/main.go
+++ cmd/podinfo/main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"flag"
+	"io/ioutil"
 	stdlog "log"
+	"os"
 	"time"
 
 	"github.com/rs/zerolog"
@@ -13,15 +15,20 @@ import (
 )
 
 var (
-	port     string
-	debug    bool
-	logLevel string
+	port                string
+	debug               bool
+	logLevel            string
+	stressCPU           int
+	stressMemory        int
+	stressMemoryPayload []byte
 )
 
 func init() {
 	flag.StringVar(&port, "port", "9898", "Port to listen on.")
 	flag.BoolVar(&debug, "debug", false, "sets log level to debug")
 	flag.StringVar(&logLevel, "logLevel", "debug", "sets log level as debug, info, warn, error, flat or panic ")
+	flag.IntVar(&stressCPU, "stressCPU", 0, "Number of CPU cores with 100% load")
+	flag.IntVar(&stressMemory, "stressMemory", 0, "MB of data to load into memory")
 }
 
 func main() {
@@ -32,6 +39,7 @@ func main() {
 	log.Debug().Msgf("Starting HTTP server on port %v", port)
 
 	stopCh := signals.SetupSignalHandler()
+	beginStressTest(stressCPU, stressMemory)
 	server.ListenAndServe(port, 5*time.Second, stopCh)
 }
 
@@ -63,4 +71,44 @@ func setLogging() {
 	// set zerolog as standard logger
 	stdlog.SetFlags(0)
 	stdlog.SetOutput(log.Logger)
+}
+
+func beginStressTest(cpus int, mem int) {
+	done := make(chan int)
+	if cpus > 0 {
+		log.Info().Msgf("Starting CPU stress, %v core(s)", cpus)
+		for i := 0; i < cpus; i++ {
+			go func() {
+				for {
+					select {
+					case <-done:
+						return
+					default:
+
+					}
+				}
+			}()
+		}
+	}
+
+	if mem > 0 {
+		path := "/tmp/podinfo.data"
+		f, err := os.Create(path)
+
+		if err != nil {
+			log.Error().Err(err).Msgf("Memory stress failed")
+		}
+
+		if err := f.Truncate(1000000 * int64(mem)); err != nil {
+			log.Error().Err(err).Msgf("Memory stress failed")
+		}
+
+		stressMemoryPayload, err = ioutil.ReadFile(path)
+		f.Close()
+		os.Remove(path)
+		if err != nil {
+			log.Error().Err(err).Msgf("Memory stress failed")
+		}
+		log.Info().Msgf("Starting memory stress, size %v", len(stressMemoryPayload))
+	}
 }
