97157694be86b3d1a278579d2e1e7a0020ef2fd6
Stefan Prodan <stefan.prodan@gmail.com>
2018-10-27 11:48:32 +0300

    Add logs to Docker build GitHub action

--- .github/actions/docker/entrypoint.sh
+++ .github/actions/docker/entrypoint.sh
@@ -15,10 +15,11 @@ if [[ "$1" == "build" ]]; then
    docker build -t ${DOCKER_IMAGE}:${DOCKER_TAG} \
    --build-arg REPOSITORY=${GITHUB_REPOSITORY} \
    --build-arg SHA=${GITHUB_SHA} -f $2 .
+   echo "Docker image tagged as ${DOCKER_IMAGE}:${DOCKER_TAG}"
 fi
 
 if [[ "$1" == "push" ]]; then
    docker push ${DOCKER_IMAGE}:${DOCKER_TAG}
+   echo "Docker image pushed to ${DOCKER_IMAGE}:${DOCKER_TAG}"
 fi
 
-echo "Docker image pushed to ${DOCKER_IMAGE}:${DOCKER_TAG}"
