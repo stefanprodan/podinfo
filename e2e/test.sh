c759f958c09fa8f2d0e1f3d801bc461856cd06b6
stefanprodan <stefan.prodan@gmail.com>
2019-08-13 11:53:28 +0300

    e2e: print logs after tests finished

--- e2e/test.sh
+++ e2e/test.sh
@@ -5,8 +5,12 @@ set -o errexit
 REPO_ROOT=$(git rev-parse --show-toplevel)
 export KUBECONFIG="$(kind get kubeconfig-path --name="kind")"
 
+function finish {
+  echo '>>> Test logs'
+  kubectl logs -l app=podinfo
+}
+trap finish EXIT
+
 echo '>>> Testing'
 helm test podinfo
 
-echo '>>> Test logs'
-kubectl logs -l app=podinfo
