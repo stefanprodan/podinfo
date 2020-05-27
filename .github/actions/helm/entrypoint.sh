55e7178dad8244a9da2631ceea06599b3811072c
stefanprodan <stefan.prodan@gmail.com>
2020-05-27 13:06:54 +0300

    Refactor Helm action

--- .github/actions/helm/entrypoint.sh
+++ .github/actions/helm/entrypoint.sh
@@ -1,10 +1,23 @@
-#!/bin/bash
-set -e
+#!/usr/bin/env bash
 
-HELM_V3=3.2.1
-BIN=/home/runner/bin
-curl -sSL https://get.helm.sh/helm-v${HELM_V3}-linux-amd64.tar.gz | tar xz
-mkdir -p ${BIN}
-cp linux-amd64/helm ${BIN}/helm
-rm -rf linux-amd64
+set -o errexit
+set -o nounset
+set -o pipefail
 
+HELM_VERSION=3.2.1
+BIN_DIR=/home/runner/bin
+
+main() {
+  mkdir -p ${BIN_DIR}
+  tmpDir=$(mktemp -d)
+
+  pushd $tmpDir >& /dev/null
+
+  curl -sSL https://get.helm.sh/helm-v${HELM_VERSION}-linux-amd64.tar.gz | tar xz
+  cp linux-amd64/helm ${BIN_DIR}/helm
+
+  popd >& /dev/null
+  rm -rf $tmpDir
+}
+
+main
