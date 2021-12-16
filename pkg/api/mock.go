3970a3a323a9b86772453033c808e12a28cb2c06
Rajat Vig <rvig@etsy.com>
2021-12-16 13:37:01 +0000

    Add noop tracer to the mockServer; migrate to go 1.17

--- pkg/api/mock.go
+++ pkg/api/mock.go
@@ -1,32 +1,34 @@
 package api
 
 import (
-	"time"
+  "go.opentelemetry.io/otel/trace"
+  "time"
 
-	"github.com/gorilla/mux"
-	"go.uber.org/zap"
+  "github.com/gorilla/mux"
+  "go.uber.org/zap"
 )
 
 func NewMockServer() *Server {
-	config := &Config{
-		Port:                      "9898",
-		HttpServerShutdownTimeout: 5 * time.Second,
-		HttpServerTimeout:         30 * time.Second,
-		BackendURL:                []string{},
-		ConfigPath:                "/config",
-		DataPath:                  "/data",
-		HttpClientTimeout:         30 * time.Second,
-		UIColor:                   "blue",
-		UIPath:                    ".ui",
-		UIMessage:                 "Greetings",
-		Hostname:                  "localhost",
-	}
+  config := &Config{
+    Port:                      "9898",
+    HttpServerShutdownTimeout: 5 * time.Second,
+    HttpServerTimeout:         30 * time.Second,
+    BackendURL:                []string{},
+    ConfigPath:                "/config",
+    DataPath:                  "/data",
+    HttpClientTimeout:         30 * time.Second,
+    UIColor:                   "blue",
+    UIPath:                    ".ui",
+    UIMessage:                 "Greetings",
+    Hostname:                  "localhost",
+  }
 
-	logger, _ := zap.NewDevelopment()
+  logger, _ := zap.NewDevelopment()
 
-	return &Server{
-		router: mux.NewRouter(),
-		logger: logger,
-		config: config,
-	}
+  return &Server{
+    router: mux.NewRouter(),
+    logger: logger,
+    config: config,
+    tracer: trace.NewNoopTracerProvider().Tracer("mock"),
+  }
 }
