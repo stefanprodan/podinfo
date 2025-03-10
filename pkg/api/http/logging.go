7cc399463ccefa6c5a0c9157d3f424aa4f59dcce
Hans van den Bogert <hansbogert@gmail.com>
2025-03-10 21:48:25 +0100

    feat(logging): add trace_id to debug log line
    ... if exists in context

--- pkg/api/http/logging.go
+++ pkg/api/http/logging.go
@@ -3,6 +3,7 @@ package http
 import (
 	"net/http"
 
+	"go.opentelemetry.io/otel/trace"
 	"go.uber.org/zap"
 )
 
@@ -18,13 +19,22 @@ func NewLoggingMiddleware(logger *zap.Logger) *LoggingMiddleware {
 
 func (m *LoggingMiddleware) Handler(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		m.logger.Debug(
-			"request started",
+		fields := []zap.Field{
 			zap.String("proto", r.Proto),
 			zap.String("uri", r.RequestURI),
 			zap.String("method", r.Method),
 			zap.String("remote", r.RemoteAddr),
 			zap.String("user-agent", r.UserAgent()),
+		}
+
+		spanCtx := trace.SpanContextFromContext(r.Context())
+		if spanCtx.HasTraceID() {
+			fields = append(fields, zap.String("trace_id", spanCtx.TraceID().String()))
+		}
+
+		m.logger.Debug(
+			"request started",
+			fields...,
 		)
 		next.ServeHTTP(w, r)
 	})
